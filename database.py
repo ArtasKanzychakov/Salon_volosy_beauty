133ce1e763b6fbd658927b19d4497c1f6bbe6b5b
ArtasKanzychakov <Artasmovie@yandex.ru>
2025-12-19 00:27:55 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,26 +1,17 @@
-import time
+"""Простое хранилище данных в оперативной памяти"""
+storage = {}
 
-class MemoryStorage:
-    def __init__(self):
-        self.data = {}
-    
-    def save(self, user_id, key, value):
-        if user_id not in self.data:
-            self.data[user_id] = {}
-        self.data[user_id][key] = value
-    
-    def get(self, user_id, key=None):
-        if user_id not in self.data:
-            return None
-        if key:
-            return self.data[user_id].get(key)
-        return self.data[user_id]
-    
-    def delete(self, user_id):
-        if user_id in self.data:
-            del self.data[user_id]
-    
-    def clear(self):
-        self.data.clear()
+def save_user_data(user_id, key, value):
+    if user_id not in storage:
+        storage[user_id] = {}
+    storage[user_id][key] = value
 
-storage = MemoryStorage()
+def get_user_data(user_id, key=None):
+    user_data = storage.get(user_id, {})
+    if key:
+        return user_data.get(key)
+    return user_data
+
+def delete_user_data(user_id):
+    if user_id in storage:
+        del storage[user_id]
