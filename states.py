fbbbd7a5159d86c55d974c13383ef7481c1c95be
ArtasKanzychakov <Artasmovie@yandex.ru>
2025-12-28 23:55:55 +0700

    Update states.py

--- states.py
+++ states.py
@@ -6,7 +6,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 class UserState(StatesGroup):
     """Состояния пользователя"""
-    MAIN_MENU = State()
     CHOOSING_CATEGORY = State()
     BODY_CHOOSING_GOAL = State()
     HAIR_CHOOSING_TYPE = State()
@@ -14,15 +13,11 @@ class UserState(StatesGroup):
     HAIR_CHOOSING_SCALP = State()
     HAIR_CHOOSING_VOLUME = State()
     HAIR_CHOOSING_COLOR = State()
-    SHOWING_RESULT = State()
 
 class AdminState(StatesGroup):
     """Состояния админ-панели"""
     WAITING_PASSWORD = State()
     ADMIN_MAIN_MENU = State()
-    ADMIN_CHOOSING_CATEGORY = State()
     ADMIN_CHOOSING_SUBCATEGORY = State()
     ADMIN_CHOOSING_PRODUCT_NAME = State()
-    ADMIN_CHOOSING_PRODUCT_KEY = State()
     ADMIN_WAITING_PHOTO = State()
-    ADMIN_CONFIRMING_UPLOAD = State()
