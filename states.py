75d9277afb5277832ab0dbbed7aa40e3d2dab5b4
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-01-28 13:24:16 +0700

    Update states.py

--- states.py
+++ states.py
@@ -18,6 +18,12 @@ class AdminState(StatesGroup):
     """Состояния админ-панели"""
     WAITING_PASSWORD = State()
     ADMIN_MAIN_MENU = State()
-    ADMIN_CHOOSING_SUBCATEGORY = State()
-    ADMIN_CHOOSING_PRODUCT_NAME = State()
-    ADMIN_WAITING_PHOTO = State()
+    ADMIN_PHOTOS_MENU = State()
+    ADMIN_BULK_UPLOAD = State()
+    ADMIN_WAITING_BULK_PHOTO = State()
+    ADMIN_CONFIRM_RESET = State()
+    
+    # Для массовой загрузки
+    ADMIN_BULK_CATEGORY = State()
+    ADMIN_BULK_SUBCATEGORY = State()
+    ADMIN_BULK_PRODUCT_LIST = State()
