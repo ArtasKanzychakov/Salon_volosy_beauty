c8c2ba84d0fed12f6576db191921ce995cd0e665
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-01 03:59:49 +0700

    Update preloaded_photos.py

--- preloaded_photos.py
+++ preloaded_photos.py
@@ -1,1 +1,53 @@
+"""
+PRELOADED_PHOTOS.PY - Предзагруженные file_id фотографий
+Добавляйте сюда file_id после загрузки через админку
+СОХРАНЯЙТЕ ЭТОТ ФАЙЛ В GIT!
+"""
 
+PRELOADED_PHOTOS = {
+    # ТЕЛО (8 фото)
+    "cream_body": "AgACAgIAAxkBAAINTml-V7UJsB2Iyo...",
+    "hydrophilic_oil": "AgACAgIAAxkBAAINuGl-abeuh63UmS...", 
+    "body_butter": "AgACAgIAAxkBAAINUml-WHePRK6QGm...",
+    "body_milk": "AgACAgIAAxkBAAINVGl-WIfTeS-HNs...",
+    "hualuronic_acid": "AgACAgIAAxkBAAINVml-WKIFJaX-Q7...",
+    "body_scrub": "AgACAgIAAxkBAAINWGl-WNh1wLytNr...",
+    "shower_gel": "AgACAgIAAxkBAAINWml-WOv_mL7P6T...",
+    
+    # ВОЛОСЫ (10 фото)
+    # Для блондинок
+    "blonde_shampoo": "AgACAgIAAxkBAAINxGl-al2LlydfHj...",
+    "blonde_conditioner": "AgACAgIAAxkBAAINYGl-WZtVCNnySP...", 
+    "blonde_mask": "AgACAgIAAxkBAAINYml-WaaUtSUzua...",
+    
+    # Спреи и масла
+    "dry_oil_spray": "AgACAgIAAxkBAAINZGl-WbPKfRDI2W...",
+    
+    # Маски
+    "mask_pink_powder": "AgACAgIAAxkBAAINZml-Wb3v4RxdUG...",
+    "mask_mother_of_pearl": "AgACAgIAAxkBAAINaGl-WccvMBfVdn...",
+    
+    # Для окрашенных волос
+    "colored_conditioner": "AgACAgIAAxkBAAINaml-WgABhFM52R...",
+    "colored_mask": "AgACAgIAAxkBAAINbGl-WjPAXiM8ip...",
+    
+    # Для тонких волос
+    "thin_hair_shampoo": "AgACAgIAAxkBAAINbml-WlwGH50yH2...",
+    
+    # ОТСУТСТВУЮТ (нужно будет дозагрузить):
+    # "perfumed_soap": "...",  # ← НОВОЕ, фото будет
+    # "colored_shampoo": "...",  # ← НОВОЕ, фото будет
+    # "thin_hair_conditioner": "...",  # ← НОВОЕ, фото будет
+    # "reconstruct_shampoo": "...",
+    # "reconstruct_mask": "...",
+    # "biolipid_spray": "...",
+    # "protein_cream": "...",
+    # "hair_milk": "...",
+    # "hair_fluid": "...",
+    # "oil_elixir": "...",
+    # "oil_concentrate": "...",
+    # "mask_cold_chocolate": "...",
+    # "mask_copper": "...",
+    # "strengthening_mask": "...",  # ← НОВОЕ, фото будет
+    # "men_shampoo": "...",  # ← НОВОЕ, фото будет
+}
